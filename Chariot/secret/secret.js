44a327da03ece1b7cd6eb272287aa88a58404c62
Raman077 <ramanprabhakar007@gmail.com>
2018-07-20 17:08:58 +0530

    first commit

---
+++ Chariot/secret/secret.js
@@ -0,0 +1,3 @@
+module.exports= {
+	database: 'mongodb://root:kalibilli7@ds141621.mlab.com:41621/cmscarto'
+}
---
+++ Chariot/server.js
@@ -0,0 +1,42 @@
+const express = require ('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const secret = require('./secret/secret.js');
+
+//connect to the database
+
+mongoose.connect(secret.database, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console,'connection error:'));
+db.once('open', function(){
+	console.log('connected');
+});
+
+//init the app
+
+const app = express();
+
+// view engine setup
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//set the public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req,res, next){
+	res.render('index',
+		{
+			title:'home'
+		});
+});
+
+app.listen(3000, function(err){
+	if(err){
+		return (err);
+	}
+	else{
+		console.log('running on port 3000');
+	}
+
+});
