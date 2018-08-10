0a43737e0a6353c677a35e6583f1dba0335c47a0
Raman077 <ramanprabhakar007@gmail.com>
2018-08-10 14:00:24 +0530

    added user models

--- Chariot/routes/admin_pages.js
+++ Chariot/routes/admin_pages.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router(); 
 
+//get pages index
+
 
 router.get('/', function(req,res, next){
 	res.send('admin area');
 });
 
-module.exports()
+
+
+//get add page
+router.get('/add-page', function(req,res, next){
+	 	const title = "";
+	 	const slug = "";
+	 	const content = "";
+
+	 	res.render('admin/add_page', {
+	 		title : title,
+	 		slug : slug,
+	 		content : content
+	 	});
+});
+
+
+module.exports= router; 
--- Chariot/server.js
+++ Chariot/server.js
@@ -2,6 +2,9 @@ const express = require ('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const secret = require('./secret/secret.js');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
 
 //connect to the database
 
@@ -20,6 +23,26 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+
+
+
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
 //set the public folder
 app.use(express.static(path.join(__dirname, 'public')));
