cc5bafe3a04e8f6bc03fd18b93960dad4d4cd636
Raman077 <ramanprabhakar007@gmail.com>
2018-12-01 01:13:15 +0530

    change some code

--- To-Do/app.js
+++ To-Do/app.js
@@ -25,6 +25,8 @@ app.locals.appname = 'Express.js Todo App'
 app.locals.moment = require('moment');
 
 app.set('port', process.env.PORT || 3000);
+
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(favicon(path.join('public/images','favicon.ico')));
@@ -32,13 +34,15 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride());
-app.use(cookieParser('CEAF3FA4-F385-49AA-8FE4-54766A9874F1'));
+app.use(cookieParser('hellothere'));
 app.use(session({
-  secret: '59B93087-78BC-4EB9-993A-A61FC844F6C9',
+  secret: 'hihello',
   resave: true,
   saveUninitialized: true
 }));
 app.use(csrf());
+
+
 
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -56,6 +60,8 @@ app.param('task_id', function(req, res, next, taskId) {
   });
 });
 
+
+
 app.get('/', routes.index);
 app.get('/tasks', tasks.list);
 app.post('/tasks', tasks.markAllCompleted)
@@ -64,13 +70,13 @@ app.post('/tasks/:task_id', tasks.markCompleted);
 app.delete('/tasks/:task_id', tasks.del);
 app.get('/tasks/completed', tasks.completed);
 
+
+
 app.all('*', function(req, res){
   res.status(404).send();
 })
-// development only
-if ('development' == app.get('env')) {
-  app.use(errorHandler());
-}
+
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
